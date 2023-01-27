ef9db2843dcb15a3cb91fe3ca931ba358b018f26
Maximiliano Tirapelli <maximilianotirapelli@gmail.com>
2023-01-27 18:27:35 -0300

    se elimina para subir una version mejorada

--- NetflixLandingPageCloneMain/Netflix-Landing-Page-Clone-main/script.js
+++
@@ -1,8 +0,0 @@
-let accordions = document.querySelectorAll('.accordion-container .accordion');
-
-accordions.forEach(acco =>{
-    acco.onclick = () =>{
-        accordions.forEach(subAcco => { subAcco.classList.remove('active') });
-        acco.classList.add('active');
-    }
-})
